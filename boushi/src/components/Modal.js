4c233c601c4baff96e8b3a135865527abc292ece
임세빈 <carefox1@naver.com>
2025-11-20 18:44:22 +0900

    .

---
+++ boushi/src/components/Modal.js
@@ -0,0 +1,13 @@
+// src/components/Modal.js
+function Modal() {
+    return (
+      <div className="model">
+        <p> boushishop by ansanthesoloist </p>
+        <p> Address : 충청남도 아산시 배방읍 호서로 </p>
+        <p> contact : 20201092@vision.hoseo.edu </p>
+      </div>
+    );
+  }
+  
+  export default Modal;
+  
